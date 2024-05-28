13714f766e6e9dd731c6864781f6e62bdd22b308
Om-Midya <dukemidya7521@gmail.com>
2024-05-28 16:41:04 +0530

    Personalization

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -8,11 +8,11 @@ const Hero = () => {
     <section className={"relative w-full h-screen mx-auto"}>
         <div className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5`}>
             <div className={"flex flex-col justify-center items-center mt-5"}>
-                <div className={"w-5 h-5 rounded-full bg-[#915eff]"}/>
-                <div className={"w-1 sm:h-80 h-40 violet-gradient"}/>
+                <div className={"w-5 h-5 rounded-full bg-[#e91e63]"}/>
+                <div className={"w-1 sm:h-80 h-40 bg-gradient-to-r from-pink-500"}/>
             </div>
             <div>
-                <h1 className={`${styles.heroHeadText} text-white`}>HI, I'm <span className={"text-[#915eff]"}>Archisman</span></h1>
+                <h1 className={`${styles.heroHeadText} text-white`}>HI, I'm <span className={"text-[#e91e63]"}>Archisman</span></h1>
                 <p className={`${styles.heroSubText} text-white-100 mt-2`}> I am an Android, Web developer with <br className={"sm:block hidden"}/> Expertise Backend Development and <br className={"sm:block hidden"}/>also
                     an AR/VR enthusiast</p>
             </div>
