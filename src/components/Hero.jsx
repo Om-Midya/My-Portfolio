630b65eeb4df73db56c881da6dc028a8c463d830
Om-Midya <dukemidya7521@gmail.com>
2024-05-30 21:12:28 +0530

    Created typing animation for the hero page and made it responsive

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,46 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import {motion} from "framer-motion";
 import {styles} from "../styles.js";
 import {ComputersCanvas} from "./canvas";
+import {TypewriterEffectSmooth} from "./ui/typewriter-effect.jsx";
+
 
 const Hero = () => {
+    const words = [
+        {
+            text: "HI,",
+            className: `${styles.heroHeadText} text-white mt-[10px]`
+        },
+        {
+            text: "I",
+            className: `${styles.heroHeadText} text-white mt-5`
+        },
+        {
+            text: "am",
+            className: `${styles.heroHeadText} text-white mt-5`
+        },
+        {
+            text: "Archisman",
+            className: `${styles.heroHeadText} text-[#e91e63] dark:text-pink-500 mt-5`,
+        },
+    ];
+
+    const[isMobile, setIsMobile] = useState(false);
+
+    useEffect(() => {
+        const mediaQuery = window.matchMedia("(max-width: 1280px)");
+        setIsMobile(mediaQuery.matches);
+
+        const handleMediaQueryChange = (event) => {
+            setIsMobile(event.matches);
+        };
+
+        mediaQuery.addEventListener("change", handleMediaQueryChange);
+
+        return () => {
+            mediaQuery.removeEventListener("change", handleMediaQueryChange);
+        };
+    }, []);
   return (
     <section className={"relative w-full h-screen mx-auto"}>
         <div className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5`}>
@@ -12,7 +49,11 @@ const Hero = () => {
                 <div className={"w-1 sm:h-80 h-40 bg-gradient-to-r from-pink-500 to-transparent"}/>
             </div>
             <div>
-                <h1 className={`${styles.heroHeadText} text-white`}>HI, I'm <span className={"text-[#e91e63]"}>Archisman</span></h1>
+                {isMobile ? <h1 className={`${styles.heroHeadText} text-white mt-5`}>HI, I'm <span
+                    className={"text-[#e91e63]"}>Archisman</span></h1> : <TypewriterEffectSmooth words={words}/>}
+
+                {/*<h1 className={`${styles.heroHeadText} text-white mt-5`}>HI, I'm <span className={"text-[#e91e63]"}>Archisman</span></h1>*/}
+
                 <p className={`${styles.heroSubText} text-white-100 mt-2`}> I am an Android, Web developer with <br className={"sm:block hidden"}/> Expertise Backend Development and <br className={"sm:block hidden"}/>also
                     an AR/VR enthusiast</p>
             </div>
