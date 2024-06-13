4875cf576862d1c3373839efa49e9e0e6909ffa4
Om-Midya <dukemidya7521@gmail.com>
2024-06-13 13:09:37 +0530

    Fixed Navbar for Projects Scrolling

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,7 +2,7 @@ import React, {useEffect, useState} from 'react'
 import {motion} from "framer-motion";
 import {styles} from "../styles.js";
 import {ComputersCanvas} from "./canvas";
-import {TypewriterEffectSmooth} from "./ui/typewriter-effect.jsx";
+import {TypewriterEffectSmooth, TypewriterEffect} from "./ui/typewriter-effect.jsx";
 
 
 const Hero = () => {
